bfae5bf56f3cc72cd7bb6c61e03c00e0b6c489c1
Madie Laine <106930581+AdieLaine@users.noreply.github.com>
2023-06-22 22:40:36 -0400

    adjusted ui

--- lnu-ai-story.py
+++ lnu-ai-story.py
@@ -73,7 +73,7 @@ st.set_page_config(
     page_title="Lnu-AI - An Indigenous AI System",
     page_icon="🪶",
     layout="centered",
-    initial_sidebar_state="auto",
+    initial_sidebar_state="collapsed",
     menu_items={
         'Get Help': 'https://github.com/AdieLaine/lnu-ai',
         'Report a bug': 'https://github.com/AdieLaine/lnu-ai/issues',
